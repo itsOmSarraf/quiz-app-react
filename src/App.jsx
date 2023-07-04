a0818587d439a0a38a5e1242f112764caa5b729d
Om Sarraf <itsomsarraf+github@gmail.com>
2023-07-05 05:29:28 +0530

    api data(questions set) fetched

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,31 @@ import React from "react";
 import Questions from "./components/Questions";
 import StartQuiz from "./components/StartQuiz";
 export default function App() {
-  const [quiz, setQuiz] = React.useState();
+  const [quiz, setQuiz] = React.useState("");
+  // const [score, setScore] = React.useState(0);
+  const [questionSet, setQuestionSet] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setQuestionSet(data.results);
+      });
+  }, [quiz]);
+
+  const [buttonClicked, setButtonClicked] = React.useState(false);
+
   function startQuiz() {
-    console.log("start quiz");
-    console.log(quiz);
-    setQuiz("not null now :P ");
-    // fetch(
-    //   "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple"
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data.results);
-    //     setQuiz(data.results);
-    //   });
+    setQuiz("not empty now :P");
+    setButtonClicked(true);
+    console.log("Quiz Started");
   }
 
   return (
     <>
       <div className="w-screen h-screen md:w-[600px] md:h-[600px] bg-[#FFFAD1] md:m-auto md:mt-9 md:rounded-3xl shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-        {!quiz ? <StartQuiz startQuiz={startQuiz} /> : <Questions />}
+        {buttonClicked ? <Questions /> : <StartQuiz startQuiz={startQuiz} />}
       </div>
     </>
   );
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,9 +1,7 @@
-export default function Questions() {
+export default function Questions(props) {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-4xl font-bold text-center text-[#293264]">
-        Quiz Questions here
-      </h1>
+      <h3></h3>
     </div>
   );
 }
