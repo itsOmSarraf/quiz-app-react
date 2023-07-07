56c00d0dba9b67fcd719669e50d7be69fe78aa1a
Om Sarraf <itsomsarraf+github@gmail.com>
2023-07-07 17:29:45 +0530

    cleaned Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -10,7 +10,6 @@ export default function Questions(props) {
           type="radio"
           name="answer"
           value={option}
-          // onChange={handleOptionChange}
           onClick={handleOptionChange}
         />
         <label htmlFor={option}>{decode(option)}</label>
@@ -20,26 +19,24 @@ export default function Questions(props) {
   function handleOptionChange(event) {
     event.preventDefault();
     let isClicked = false;
-    // Update the selected option in the parent component's state
-    // props.onChange(event.target.value);
+
     console.log(event.target.value, props.correctAns, props.question);
     if (event.target.value === props.correctAns) {
       console.log("Correct Answer");
-      // event.target.parentElement.classList.add("bg-green-400");
+
       isClicked = true;
     } else {
       console.log("Incorrect Answer");
-      // event.target.parentElement.classList.add("bg-red-400");
+
       isClicked = true;
     }
   }
-  // console.log(response);
+
   return (
     <>
       <form className="px-5 py-3">
         <legend>{decode(props.question)}</legend>
         <div className="flex gap-3">{optionsArr}</div>
-        {/* <hr className="mt-2" /> */}
       </form>
     </>
   );
