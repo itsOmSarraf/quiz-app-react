84c7410e553b9d43f9a4a601f6890b0dbb5620ed
Om Sarraf <itsomsarraf+github@gmail.com>
2023-07-09 18:44:49 +0530

    works well

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { decode } from "html-entities";
+import shuffle from "lodash/shuffle";
 
 export let responseArr = [];
 
 export default function Questions(props) {
   const [selectedOption, setSelectedOption] = useState("");
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    // Ensure that props.correctAns is not included in props.incorrectAns
+    const incorrectAnswers = props.incorrectAns.filter(
+      (ans) => ans !== props.correctAns
+    );
+
+    // Then shuffle
+    setOptions(shuffle(incorrectAnswers.concat(props.correctAns)));
+  }, [props.incorrectAns, props.correctAns]);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -26,7 +38,7 @@ export default function Questions(props) {
     responseArr.push(responseObj);
   };
 
-  const optionsArr = props.incorrectAns.map((option, index) => {
+  const optionsArr = options.map((option, index) => {
     const isSelected = option === selectedOption;
     let finalStyleOption = {};
     if (props.revealed) {
@@ -41,7 +53,7 @@ export default function Questions(props) {
     } else {
       finalStyleOption = isSelected ? "bg-orange-200" : "bg-[#9dacf5]";
     }
-    const optionStyle = `w-full my-0 text-center rounded-xl py-1 px-3 cursor-pointer ${finalStyleOption}`;
+    const optionStyle = `my-3 md:my-0 text-center rounded-xl py-1 px-3 bg-[#9dacf5] cursor-pointer ${finalStyleOption}`;
 
     let uniqueId = `${props.question}_${index}`; // Unique Id here
 
@@ -65,7 +77,7 @@ export default function Questions(props) {
     <>
       <form className="px-5 py-3">
         <legend className="cursor-pointer">{decode(props.question)}</legend>
-        <div className="cursor-pointer h-full flex-wrap md:flex gap-3">
+        <div className="cursor-pointer flex flex-wrap md:flex gap-3">
           {optionsArr}
         </div>
       </form>
